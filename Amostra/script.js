9c814770da06fdfb94e37ad619d3500952dc6221
goncalomag <xipinho15.xipinho15@gmail.com>
2024-06-26 12:56:40 +0100

    Página de Produtos Individual | Alteração dos caminhos dos ficheiros | Página de HomePage na pasta principal

---
+++ Amostra/script.js
@@ -0,0 +1,28 @@
+function muda(){
+    window.location.href = '/Amostra/index.html';
+}
+
+const image = document.getElementById( 'productImg' );
+const btn = document.getElementsByClassName( 'btn' );
+
+btn[0].addEventListener( 'click', function(){
+    image.src='./imagens/cinza.png';
+    for ( bt of btn){
+        bt.classList.remove( 'active' );
+    }
+    this.classList.add( 'active' )
+});
+btn[1].addEventListener( 'click', function(){
+    image.src='./imagens/azul.png'
+    for ( bt of btn){
+        bt.classList.remove( 'active' );
+    }
+    this.classList.add( 'active' )
+});
+btn[2].addEventListener( 'click', function(){
+    image.src='./imagens/vermelho.png'
+    for ( bt of btn){
+        bt.classList.remove( 'active' );
+    }
+    this.classList.add( 'active' )
+});
