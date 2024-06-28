5e106f06bbf73e136fcdec9340ba8931390b4427
goncalomag <xipinho15.xipinho15@gmail.com>
2024-06-28 11:59:39 +0100

    Configuração do media para todos os dispositivos v.02

--- Listagem/script.js
+++ Listagem/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
   const simbolo = document.getElementById('filter');
+  const body = document.getElementById('body');
   const filtro = document.getElementById('filtro');
   const galeria = document.getElementById('galeria');
   const fechaFiltro = document.getElementById('fechaFiltro');
@@ -7,12 +8,17 @@ document.addEventListener("DOMContentLoaded", function(){
   simbolo.addEventListener('click', () =>{
     filtro.classList.toggle('hide');
     galeria.classList.toggle('pequena');
+    body.style.overflow="hidden";
+    galeria.style.overflow="hidden";
+
   });
 
 
   fechaFiltro.addEventListener('click', ()=> {
     filtro.classList.toggle('hide');
     galeria.classList.toggle('pequena');
+    body.style.overflow="auto";
+    galeria.style.overflow="none";
   })
 });
 
