fdaa4d76e69762657dcb5026353c9aa5d6532c0c
goncalomag <xipinho15.xipinho15@gmail.com>
2024-06-28 16:35:43 +0100

    Correção de Bugs na estrela de favoritos | Correção de bugs do botão dark nos media

--- Amostra/scriptFavorito.js
+++ Amostra/scriptFavorito.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {/* faz com que atribuia as variaveis e execute o codigo apenas depois da pagina toda carregada */
     const star = document.getElementById('star');
 
-    star.addEventListener('click', () => {
-        star.classList.toggle('teste');
-    });
 });
+function teste(){
+    star.classList.toggle('teste');
+};
+
